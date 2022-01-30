a5517f0024572589cd4cadbf07a2707a42862d58
l0ckD2wN <Gunmetaltiger35@gmx.de>
2022-01-30 23:50:16 +0100

    multiple_bots_input.py

---
+++ multiple_bots_input.py
@@ -0,0 +1,14 @@
+import kahoot
+
+counter = 0
+name = input("Enter your bots names: ")
+code = input("Enter the code: ")
+
+
+while counter < 10:
+    bot = kahoot.client()
+    bot.join(code, name + str(counter))
+    def joinHandle():
+        print("Bot joined")
+    bot.on("joined", joinHandle)
+    counter += 1
